d923cdc237a82a420c7cc14697a88faa03bae0e1
Chris Marlow <christopher.marlow@shopify.com>
2019-05-06 00:02:50 -0400

    Extracting post summary to a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "components/layout"
 import SEO from "components/seo"
+import PostSummary from "components/post-summary"
 import { rhythm } from "utils/typography"
 
 class BlogIndex extends React.Component {
@@ -16,28 +17,9 @@ class BlogIndex extends React.Component {
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
-        })}
+        {posts.map(({ node }) =>
+          <PostSummary node={node} />
+        )}
       </Layout>
     )
   }
