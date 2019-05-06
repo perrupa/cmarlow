7cf09ea58badc63eb66d661a853536dfceee7fa4
Chris Marlow <christopher.marlow@shopify.com>
2019-05-05 22:01:21 -0400

    Removing bio

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,5 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
@@ -35,7 +33,6 @@ class BlogPostTemplate extends React.Component {
             marginBottom: rhythm(1),
           }}
         />
-        <Bio />
 
         <ul
           style={{
