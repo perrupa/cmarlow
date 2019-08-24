754dfc16040592d7e50c75d9c4afe03f59acece5
Chris Marlow <christopher.marlow@shopify.com>
2019-08-24 18:27:54 -0400

    Styling blog links # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch syntax-highlighting # Changes to be committed: #	modified:   src/templates/blog-post.js #

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,56 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import styled from "styled-components"
+import { graphql } from "gatsby"
 import Layout from "components/layout"
+import { Link } from "components/link"
 import SEO from "components/seo"
 import { rhythm, scale } from "utils/typography"
 
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 3em auto;
+`
+
+const Links = ({ next, previous }) => {
+  return (
+    <LinkContainer>
+      <span>
+        {previous && (
+          <Link to={previous.fields.slug} rel="prev">
+            ← {previous.frontmatter.title}
+          </Link>
+        )}
+      </span>
+      <span>
+        {next && (
+          <Link to={next.fields.slug} rel="next">
+            {next.frontmatter.title} →
+          </Link>
+        )}
+      </span>
+    </LinkContainer>
+  )
+}
+
+const Post = ({ post }) => {
+  return (
+    <section>
+      <h1>{post.frontmatter.title}</h1>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}
+      >
+        {post.frontmatter.date}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </section>
+  )
+}
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
@@ -16,48 +63,8 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+        <Post post={post} />
+        <Links next={next} previous={previous} />
       </Layout>
     )
   }
