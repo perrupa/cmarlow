6b144a0e3aab234d5b8113e2e8a11ed405a27b65
Chris Marlow <christopher.marlow@shopify.com>
2019-09-14 16:55:21 -0400

    Overide ugly code block style # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is ahead of 'origin/master' by 1 commit. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   src/components/container.js # # Changes not staged for commit: #	modified:   src/components/post-summary.js #	modified:   src/templates/blog-post.js #

--- src/components/container.js
+++ src/components/container.js
@@ -3,4 +3,11 @@ import styled from "styled-components"
 export const Container = styled.section`
   max-width: 590px;
   margin: 2em auto;
+
+  // is there a better place to put this?
+  pre[class*="language-"] {
+    font-size: 90%;
+    border-radius: 0;
+    border: 0;
+  }
 `
