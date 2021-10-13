e7246e50900abf92941def530d71684d24d24915
Chris Marlow <christopher.marlow@shopify.com>
2021-10-13 14:40:31 -0400

    Update blog's max-width
    section heading margins updated

--- src/components/BlogContainer.js
+++ src/components/BlogContainer.js
@@ -1,6 +1,15 @@
 import styled from "styled-components"
 
 export const BlogContainer = styled.article`
-  width: 80%;
-  margin: 3em auto;
+  max-width: 60rem;
+  margin: 5em auto;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: 2em;
+  }
 `
