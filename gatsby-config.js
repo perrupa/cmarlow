92af2f5281a96934de66a68b663275eff9e9c2d5
Chris Marlow <christopher.marlow@shopify.com>
2022-05-29 18:19:34 -0400

    Add GoogleAnalytics trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,7 +92,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-9VVVZFCZRK`,
       },
     },
     `gatsby-plugin-feed`,
